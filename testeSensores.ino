2ea68923b3eac5dd4cfe025d7f7b074eeda8a40f
Dandara Torquato <157934500+torquArt@users.noreply.github.com>
2025-12-25 08:18:09 -0300

    Refactor sensor code for readability and consistency

--- testeSensores.ino
+++ testeSensores.ino
@@ -1,13 +1,7 @@
 //código utilizado para verificar o funcionamento dos sensores e seus respectivos valores de leitura analógica
 
-char dedos[5][10] = {
-  "polegar",
-  "indicador",
-  "medio",
-  "anelar",
-  "minimo",};
-  
-int pino[5] = {A0, A1, A2, A3, A4};
+char dedos[5][10] = {"polegar", "indicador", "medio", "anelar","minimo"};
+int pinosSensores[5] = {A0, A1, A2, A3, A4};
 
 void setup() {
 Serial.begin(9600);
@@ -15,11 +9,7 @@ Serial.begin(9600);
 
 void loop() {
 for (int i = 0; i < 5; i++){
-Serial.print(dedos[i]);
-Serial.print(" ");
-Serial.print(analogRead(pino[i]));
-Serial.print(" // ");
+Serial.print(dedos[i]); Serial.print(" "); Serial.print(analogRead(pinosSensores[i])); Serial.print(" // "); //Imprime no monitor serial o dedo que está sendo verificado seguido por sua respectiva leitura analógica.
 }
 Serial.println();
-
 }
